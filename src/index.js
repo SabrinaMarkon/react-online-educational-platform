21339d86b30cf4f3a3389cd852c6e269f03a7645
Sabrina Markon <phpsitescripts@outlook.com>
2018-07-31 01:26:35 -0600

    fix bug in webpack.config.js making babel not work

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Index = () => {
-    return <div>Hello Cats!</div>
+    return <div>Online Educational Platform</div>
 }
 
 ReactDOM.render(<Index />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
+const path = require("path");
+
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: babel-loader
-                }
-            }
-        ]
-    }
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+};
