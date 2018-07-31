f23d5e0cd1f7ad05e5b11b0e6aa18f3b32f17e10
Sabrina Markon <phpsitescripts@outlook.com>
2018-07-31 02:49:45 -0600

    Add debug.css and webpack config for .css

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 import '../styles/App.scss';
+// import '../styles/debug.css';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>React Online Educational Platform</h1>
-                <h2>I love cats</h2>
+            <div className="hero has-background-black">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1>React Online Educational Platform</h1>
+                        <h2>I love cats</h2>
+                    </div>
+                </div>
             </div>
         );
     }
