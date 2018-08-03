5af5747735dd713e5029981b2dac2cc2e6c50d87
Sabrina Markon <phpsitescripts@outlook.com>
2018-08-02 23:48:24 -0600

    Test main route renders hero title  correctly

---
+++ __tests__/App.test.js
@@ -0,0 +1,40 @@
+// for Jest snapshots:
+import renderer from 'react-test-renderer';
+
+// jest.setTimeout(30000);
+const puppeteer = require('puppeteer');
+
+describe('Addition', () => {
+    it('knows that 2 plus 2 make 4', () => {
+        expect(2 + 2).toBe(4);
+    });
+});
+
+/* set up our browser and how we want to use it.
+We can test different browser options */
+const isDebugging = ()=> {
+    const debugging_mode = {
+      headless: false,
+      slowMo: 250,
+      devtools: true
+    }
+    return process.env.NODE_ENV === 'debug' ? debugging_mode : {}
+  }
+/* We don't want to create a new browser and page for every single test
+so put browser and page outside the test.
+The two vars below are the browser and page instances */
+let browser;
+let page;
+beforeAll(async() => {
+    browser = await puppeteer.launch(isDebugging());
+    page = await browser.newPage();
+     /* use page.goto to tell puppeteer where to navigate inside the browser */
+     await page.goto('http://localhost:8080');
+    page.setViewport({ width: 500, height: 2400 });
+});
+describe('When the page loads', () => {
+    test('it loads the correct h1 title in the content', async() => {
+       const html = await page.$eval('h1', e => e.innerHTML);
+       expect(html).toBe('React Online Educational Platform');
+    }, 15000);
+});
--- src/components/App.js
+++ src/components/App.js
@@ -6,31 +6,38 @@ import '../styles/App.scss';
 class App extends Component {
     render() {
         return (
-        <div class="container">
-            <div className="hero has-background-black">
+        <div>
+            <div className="hero">
                 <div className="hero-body">
                     <div className="container">
                         <h1>React Online Educational Platform</h1>
                     </div>
                 </div>
             </div>
-            <div class="columns">
-                <div class="column">
-                    <div class="notification">
-                    One Bulma column
+            <section className="section has-background-light">
+                <div className="container">
+                    <div className="columns">
+                        <div className="column">
+                            <div className="notification is-primary">
+                                <h1 className="title is-size-4">TItle 1</h1>
+                                <p className="is-size-5">One Bulma column</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="notification is-success">
+                                <h1 className="title is-size-4">TItle 1</h1>
+                                <p className="is-size-5">Two Bulma column</p>
+                            </div>
+                        </div>
+                        <div className="column">
+                            <div className="notification is-info">
+                                <h1 className="title is-size-4">TItle 1</h1>
+                                <p className="is-size-5">Three Bulma column</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="column">
-                    <div class="notification">
-                    Two Bulma column
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="notification">
-                    Three Bulma column
-                    </div>
-                </div>
-            </div>
+            </section>
         </div>
         );
     }
